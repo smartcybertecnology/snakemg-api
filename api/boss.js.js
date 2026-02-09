baf68bda2131e9042697627dd2936b44720fe4c8
smartcybertecnology <smartcybertecnology@gmail.com>
2026-02-09 20:10:54 -0300

    Add files via upload

---
+++ api/boss.js.js
@@ -0,0 +1,32 @@
+import { WebSocketServer } from "ws";
+
+let boss = { x: 100, y: 100, hp: 100 };
+let minions = [
+  { x: 120, y: 100 },
+  { x: 80, y: 100 }
+];
+
+export default function handler(req, res) {
+  if (!global.wss) {
+    global.wss = new WebSocketServer({ noServer: true });
+
+    // Atualização periódica
+    setInterval(() => {
+      boss.x += Math.random() > 0.5 ? 1 : -1;
+      boss.y += Math.random() > 0.5 ? 1 : -1;
+      minions.forEach(m => {
+        m.x += Math.random() > 0.5 ? 1 : -1;
+        m.y += Math.random() > 0.5 ? 1 : -1;
+      });
+
+      const state = { boss, minions };
+      global.wss.clients.forEach(client => {
+        if (client.readyState === 1) {
+          client.send(JSON.stringify(state));
+        }
+      });
+    }, 200);
+  }
+
+  res.status(200).send("Boss server running");
+}
